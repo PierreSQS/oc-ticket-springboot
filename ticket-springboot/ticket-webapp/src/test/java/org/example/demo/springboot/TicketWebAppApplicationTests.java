cc119bc215ea0b28ce2b6e08a2ff689868233b36
PierrotSQS <nomo_messina@web.de>
2021-04-11 12:45:05 +0200

    Commit15: refined Test to real check the Context-Load

--- ticket-springboot/ticket-webapp/src/test/java/org/example/demo/springboot/TicketWebAppApplicationTests.java
+++ ticket-springboot/ticket-webapp/src/test/java/org/example/demo/springboot/TicketWebAppApplicationTests.java
@@ -1,13 +1,24 @@
 package org.example.demo.springboot;
 
+import org.example.demo.springboot.business.contract.ProjetManager;
+import org.example.demo.springboot.configuration.SpringConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class TicketWebAppApplicationTests {
+@ContextConfiguration(classes = {SpringConfiguration.class})
+class TicketWebAppApplicationTests {
+
+    @Autowired
+    ProjetManager projetManager;
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        assertThat(projetManager).isNotNull();
     }
 
 }
