99a5e2fc285a1d57022520332fd1035019dff63b
PierrotSQS <nomo_messina@web.de>
2021-04-11 11:39:43 +0200

    Commit13: Fixes the issue on previous commit (s. 52c0c12d)

--- ticket-springboot/ticket-webapp/src/test/java/org/example/demo/springboot/controllers/ProjetControllerTest.java
+++ ticket-springboot/ticket-webapp/src/test/java/org/example/demo/springboot/controllers/ProjetControllerTest.java
@@ -1,14 +1,17 @@
 package org.example.demo.springboot.controllers;
 
+import org.example.demo.springboot.configuration.SpringConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest(controllers = {ProjetController.class})
+@ContextConfiguration(classes = {SpringConfiguration.class})
 public class ProjetControllerTest {
 
     @Autowired
