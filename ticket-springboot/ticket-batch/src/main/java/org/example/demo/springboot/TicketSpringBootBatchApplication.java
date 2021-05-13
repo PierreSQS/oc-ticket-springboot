a31679dfd530df1a598621626a003fed8e43394b
PierrotSQS <nomo_messina@web.de>
2021-05-13 21:44:42 +0200

    Commit20: Hurrahhhh!! Now the Batch Submodule is SpringBoot conform!!!!.

--- ticket-springboot/ticket-batch/src/main/java/org/example/demo/springboot/TicketSpringBootBatchApplication.java
+++ ticket-springboot/ticket-batch/src/main/java/org/example/demo/springboot/TicketSpringBootBatchApplication.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.demo.springboot.business.factory.contract.ManagerFactory;
 import org.example.demo.springboot.model.bean.ticket.TicketStatut;
 import org.example.demo.springboot.model.exception.TechnicalException;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -29,7 +31,7 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @SpringBootApplication
-@ImportResource(locations = "classpath:org/example/demo/springboot/batch/batchContext.xml")
+@ImportResource(locations = {"org/example/demo/springboot/batch/batchContext.xml"})
 public class TicketSpringBootBatchApplication {
 
     /**
@@ -38,49 +40,54 @@ public class TicketSpringBootBatchApplication {
      * @param pArgs the input arguments
      */
     public static void main(String[] pArgs) {
-        
-    	ConfigurableApplicationContext appCtx = 
-        		new AnnotationConfigApplicationContext("org.example.demo.springboot");
+        SpringApplication.run(TicketSpringBootBatchApplication.class, pArgs);
+    }
 
-        try (appCtx) {
-            ManagerFactory vMgrFct = appCtx.getBean("managerFactory", ManagerFactory.class);
-            FileSystemResource vRes = appCtx.getBean("resourceTicket", FileSystemResource.class);
-            if (pArgs.length < 1) {
-                throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
+    @Bean
+    public CommandLineRunner runBatch(ConfigurableApplicationContext appCtx){
+
+        return args -> {
+            try (appCtx) {
+                ManagerFactory vMgrFct = appCtx.getBean("managerFactory", ManagerFactory.class);
+                FileSystemResource vRes = appCtx.getBean("resourceTicket", FileSystemResource.class);
+                if (args.length < 1) {
+                    throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
+                }
+
+                String vTraitementId = args[0];
+
+                if ("ExportTicketStatus".equals(vTraitementId)) {
+
+                    log.info("Execution du traitement : ExportTicketStatus");
+
+                    // le module batch ne parle qu'avec le module business donc pas
+                    // d'appel direct d'un DAO !!!
+                    List<String> statuts = vMgrFct.getTicketManager().getListTicketStatut()
+                            .stream()
+                            .map(TicketStatut::toString)
+                            .collect(Collectors.toList());
+
+                    writeStatut(vRes, statuts);
+
+                    log.info("###### Nom du fichier: " + vRes.getFilename());
+
+
+                    int beansCnt = appCtx.getBeanDefinitionCount();
+                    log.info("Number of Beans: " + beansCnt);
+
+                    log.info("The created Beans... ");
+                    Arrays.asList(appCtx.getBeanDefinitionNames())
+//                            .stream().sorted()
+                            .forEach(log::info);
+
+                } else {
+                    throw new TechnicalException("Traitement inconnu : " + vTraitementId);
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                System.exit(1);
             }
-
-            String vTraitementId = pArgs[0];
-
-            if ("ExportTicketStatus".equals(vTraitementId)) {
-
-                log.info("Execution du traitement : ExportTicketStatus");
-
-                // le module batch ne parle qu'avec le module business donc pas
-                // d'appel direct d'un DAO !!!
-                List<String> statuts = vMgrFct.getTicketManager().getListTicketStatut()
-                        .stream()
-                        .map(TicketStatut::toString)
-                        .collect(Collectors.toList());
-
-                writeStatut(vRes, statuts);
-
-                log.info("###### Nom du fichier: " + vRes.getFilename());
-
-
-                int beansCnt = appCtx.getBeanDefinitionCount();
-                log.info("Number of Beans: " + beansCnt);
-
-                log.info("The created Beans... ");
-                Arrays.asList(appCtx.getBeanDefinitionNames())
-                        .forEach(log::info);
-
-            } else {
-                throw new TechnicalException("Traitement inconnu : " + vTraitementId);
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            System.exit(1);
-        }
+        };
     }
 
 	private static void writeStatut(Resource vRes, List<String> lines) throws IOException {
